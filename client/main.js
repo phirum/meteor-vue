2e42047739260a6577ede8c16ae963c51a63b073
phirumsim <simphirum@gmail.com>
2017-07-24 16:25:44 +0700

    first commit

---
+++ client/main.js
@@ -0,0 +1,23 @@
+// Libs
+import { Meteor } from 'meteor/meteor';
+import Vue from 'vue';
+import { Accounts } from 'meteor/accounts-base';
+
+Accounts.ui.config({
+  passwordSignupFields: 'USERNAME_AND_EMAIL',
+})
+
+import VueTracker from 'vue-meteor-tracker';
+Vue.use(VueTracker);
+
+import VueMeta from 'vue-meta';
+Vue.use(VueMeta);
+
+// Main app
+import App from '/imports/ui/App.vue';
+
+Meteor.startup(() => {
+  new Vue({
+    render: h => h(App),
+  }).$mount('app');
+});
